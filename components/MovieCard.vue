170d7c8732f8bccdd18472a7ec9f362f082112c1
sungkyo.lee <sungkyo.lee@lemonhc.com>
2021-05-27 18:14:12 +0900

    commit

--- components/MovieCard.vue
+++ components/MovieCard.vue
@@ -1,40 +1,12 @@
 <template>
-   <div class="col mb-5">
-        <div class="card h-100">
-            <!-- Sale badge-->
-            <div v-if="isSale" class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
-
-            <!-- Product image-->
-            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-            <!-- Product details-->
-            <div class="card-body p-4">
-                <div class="text-center">
-                    <!-- Product name-->
-                    <h5 class="fw-bolder">{{productName}}</h5>
-                     <!-- Product reviews-->
-                    <div class="d-flex justify-content-center small text-warning mb-2">
-                        <div class="bi-star-fill"></div>
-                        <div class="bi-star-fill"></div>
-                        <div class="bi-star-fill"></div>
-                        <div class="bi-star-fill"></div>
-                        <div class="bi-star-fill"></div>
-                    </div>
-                    <!-- Product price-->
-                    <span v-if="beforePrice !== ''" class="text-muted text-decoration-line-through">{{beforePrice}}</span>
-                    {{productPrice}}
-                </div>
-            </div>
-            <!-- Product actions-->
-            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                <div class="text-center"><my-button :link="'#'">View options</my-button></div>
-            </div>
-        </div>
+    <div>
+        
     </div>
 </template>
 
 <script>
 export default {
-    props: ['isSale', 'src', 'productName', 'productPrice', 'stars', 'beforePrice']
+   
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -6,48 +6,33 @@
         <section class="py-5">
             <div class="container px-4 px-lg-5 mt-5">
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    <MovieCard 
-                        :isSale=false 
-                        :src="'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'" 
-                        :productName="'Fancy Product'" 
-                        :beforePrice="''" 
-                        :productPrice="'$40.00 - $80.00'" 
-                        :stars="0"/>
-                    <MovieCard 
-                        :isSale=true 
-                        :src="'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'" 
-                        :productName="'Special Item'" 
-                        :beforePrice="'$18.00'"  
-                        :productPrice="'$18.00'" 
-                        :stars="5"/>
-                    <MovieCard 
-                        :isSale=true 
-                        :src="'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'" 
-                        :productName="'Sale Item'" 
-                        :beforePrice="'$50.00'"  
-                        :productPrice="'$25.00'" 
-                        :stars="0"/>
-                    <MovieCard 
-                        :isSale=false 
-                        :src="'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'" 
-                        :productName="'Popular Item'" 
-                        :beforePrice="''"  
-                        :productPrice="'$40.00'" 
-                        :stars="5"/>
-                    <MovieCard 
-                        :isSale=false 
-                        :src="'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'" 
-                        :productName="'Fancy Product'" 
-                        :beforePrice="''" 
-                        :productPrice="'$40.00 - $80.00'" 
-                        :stars="0"/>
-                    <MovieCard 
-                        :isSale=true 
-                        :src="'https://dummyimage.com/450x300/dee2e6/6c757d.jpg'" 
-                        :productName="'Special Item'" 
-                        :beforePrice="'$18.00'"  
-                        :productPrice="'$18.00'" 
-                        :stars="5"/>
+                    <div v-for="item in movieList.dailyBoxOfficeList" class="col mb-5" v-bind:key="item.movieCd">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">{{item.movieNm}}</h5>
+                                    <!-- Product reviews-->
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    <!-- Product price-->
+                                    {{item.showCnt}}
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><my-button :link="'#'">View Details</my-button></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
@@ -64,7 +49,25 @@ import MovieCard from '~/components/MovieCard.vue'
 
 
 export default Vue.extend({
-  components: { MovieCard, Navigation, Footer, Header },})
+    data: function() {
+        return {
+            movieList : {}
+        }
+    },
+    components: { MovieCard, Navigation, Footer, Header },
+    created() {
+        this.getDailyBoxOfficeMoives();
+    },
+    mounted() {
+        
+    },
+    methods: {
+        async getDailyBoxOfficeMoives() {
+            const movieList = await this.$axios.$get('http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=ecbf42fb330b5ce4a401178c80e43323&targetDt=20210525');
+            this.movieList = movieList;
+        }
+    }
+})
 </script>
 
 <style>
